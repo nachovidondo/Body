0e09b273e5ec69a6ed51962cdf56f8b447a10da5
NachoVidondo <ignaciovidondo@hotmail.com>
2021-10-12 12:32:54 -0300

    Modifications

---
+++ Bodyworkz/MainApp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+  
+from django import forms
+from django.forms import widgets
+from django.forms.widgets import TextInput
+
+class Contactform(forms.Form):
+    name = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Name'}
+    ))
+    email = forms.EmailField(required=True, widget=forms.TextInput(
+        attrs={'class':'form-control','placeholder':'Email'}
+    ))
+    content = forms.CharField(required= True, widget=forms.Textarea(
+        attrs={"rows":5, "cols":20, 'class':'form-control','placeholder':'Message'}
+    ))
--- Bodyworkz/MainApp/views.py
+++ Bodyworkz/MainApp/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render,reverse , redirect
+from .forms  import Contactform
+from django.core.mail import EmailMessage
 
-# Create your views here.
+
+#Index
+def index(request):
+
+    return render (request,'index.html')
+
+#Contact
+
+def contacto(request):
+    contact_form = Contactform()
+    if request.method == "POST":  
+        contact_form = Contactform(data=request.POST)
+        if contact_form.is_valid(): 
+            name= request.POST.get('name')
+            email= request.POST.get('email')
+            content= request.POST.get('content')
+            mail = EmailMessage(
+                "Bodyworkz Massage : Nuevo Mensaje de Contacto ",
+                "De {} {}\n\nEscribio:\n\n {}".format(name ,email,content),
+                "circuloin.com", ["ignaciovidondo@hotmail.com"],
+                reply_to = [email]
+                )
+            try:
+                mail.send() #Si esta todo ok redireccionar
+                return redirect(reverse("automatic")+"?ok")
+                 
+                 
+            
+            except:
+                return redirect(reverse("contacto")+"?fail")
+            
+    return render (request, 'contact.html', {'form':contact_form })
+
+#Automatic message after contact us
+def automatic(request):
+    return render (request, 'automatic.html') 
